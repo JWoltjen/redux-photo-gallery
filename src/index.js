e56ee32e5a07286de93abbf48c195ead8ff2c3f5
Jeff Woltjen (he/him) <Jeff.Woltjen@gmail.com>
2021-11-22 13:32:41 -0600

    configure store

--- src/index.js
+++ src/index.js
@@ -3,9 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import {Provider} from 'react-redux'
+import {configureStore} from '@reduxjs/toolkit'
+
+const store = configureStore({
+  reducer: {}
+}); 
+
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
