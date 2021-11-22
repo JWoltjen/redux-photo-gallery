c13ce8e3481dbde83e499c3b7126a662dcb1f70a
Jeff Woltjen (he/him) <Jeff.Woltjen@gmail.com>
2021-11-22 14:33:42 -0600

    map over photos in display

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
 import './App.css';
+import {useEffect} from 'react' 
+import {useDispatch, useSelector} from 'react-redux'
+import {getPhotos} from './galleryState'; 
+
 
 function App() {
+  const dispatch = useDispatch(); 
+  const photos = useSelector(state => state.gallery.photos)
+
+  useEffect(() => {
+    dispatch(getPhotos()); 
+  }, [dispatch]); 
+  
   return (
     <div className="App">
     
